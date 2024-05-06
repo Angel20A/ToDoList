8513a3afd1dfde4a04354b56f72d13a7e0f86e43
Angel20A <168486862+Angel20A@users.noreply.github.com>
2024-05-06 16:16:18 -0600

    Add files via upload

---
+++ To-do list/Servicios/Contrato/IUsuarioService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using To_do_list.Models;
+
+namespace To_do_list.Servicios.Contrato
+{
+    public interface IUsuarioService //Interfaz
+    {
+        //Declarar los métodos que se van a estar utilizando
+
+        Task<Usuario> GetUsuario(string correo, string contrasena); //Va a devolver un usuario a través de un correo y una contraseña
+
+        Task<Usuario> SaveUsuario(Usuario usuario); //Guardar un usuario
+    }
+}
---
+++ To-do list/Servicios/Implementacion/UsuarioService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using To_do_list.Models;
+using To_do_list.Servicios.Contrato;
+
+namespace To_do_list.Servicios.Implementacion
+{
+    public class UsuarioService : IUsuarioService //Añadir la herencia del contratro que se creó
+    {
+        private readonly ToDoListDbContext _dbContext;
+
+        public UsuarioService(ToDoListDbContext dbContext) //Se crea el constructor
+        {
+            _dbContext = dbContext;
+        }
+
+        //Nos devuelve los métodos que estaban en la interfaz IUsuarioService
+        public async Task<Usuario> GetUsuario(string correo, string contrasena)
+        {
+            Usuario usuarioEncontrado = await _dbContext.Usuarios.Where(u => u.Correo == correo && u.Contrasena == contrasena)
+                .FirstOrDefaultAsync();
+
+            return usuarioEncontrado;
+        }
+
+        public async Task<Usuario> SaveUsuario(Usuario usuario)
+        {
+            _dbContext.Usuarios.Add(usuario);
+            await _dbContext.SaveChangesAsync();
+
+            return usuario;
+        }
+    }
+}
