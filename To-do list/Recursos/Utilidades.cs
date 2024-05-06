c6634155d8fc44beea994f2e618f72fabe77531d
Angel20A <168486862+Angel20A@users.noreply.github.com>
2024-05-06 16:16:02 -0600

    Add files via upload

---
+++ To-do list/Recursos/Utilidades.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography; 
+using System.Text;
+
+namespace To_do_list.Recursos
+{
+    public class Utilidades
+    {
+        //Método para encriptar la clave
+        public static string EncriptarClave(string clave)
+        {
+            StringBuilder sb = new StringBuilder(); 
+
+            using (SHA256 hash  = SHA256Managed.Create()) //hash nos permite encriptar
+            {
+                Encoding enc = Encoding.UTF8; //codificación
+
+                byte[] result = hash.ComputeHash(enc.GetBytes(clave)); 
+
+                foreach(byte b in result) //iterar los elementos que se encuentran en el result
+                {
+                    sb.Append(b.ToString("x2")); //concatenar el resultado. x2 nos indica que el formato se debe formatear en hexadecimal
+                }
+            }
+            return sb.ToString(); //devolver variable sb en string
+        }
+    }
+}
